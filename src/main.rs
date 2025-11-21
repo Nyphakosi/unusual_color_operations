42c2a300f69e54316375a1bb8a9e00ededbe7b9c
Nyphakosi <nyphakosi@gmail.com>
2025-11-20 16:45:37 -0800

    linear_piece_any can handle inputs of len 0 now

--- src/main.rs
+++ src/main.rs
@@ -77,8 +77,11 @@ fn angle_reflect(reflect_angle: f32) -> impl Fn(f32)->f32 + Send + 'static {
     // and with algebra, 2A-C mod 360
     move |x| (2.0*reflect_angle - x).rem_euclid(360.0)
 }
-fn linear_piece_any(points: Vec<(f32, f32)>) -> impl Fn(f32)->f32 + Send + 'static { // assumes input is at least length 1
+fn linear_piece_any(points: Vec<(f32, f32)>) -> impl Fn(f32)->f32 + Send + 'static {
     let mut points = points.clone();
+    if points.is_empty() {
+        points.push((180., 180.));
+    }
     points.sort_by(|a, b| a.0.total_cmp(&b.0)); // order by x coordinate
     points.insert(0, (points[points.len()-1].0 - 360.0, points[points.len()-1].1 - 360.0)); // shift the last point leftward to before the modulo bounds
     points.push((points[0].0 + 360.0, points[0].1 + 360.0)); // shift the first point rightward to after the modulo bounds
